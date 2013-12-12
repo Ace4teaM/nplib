a0255b573aaf31ebe13d4df2b209bda68df6f7b0
Thomas AUGUEY <contact@aceteam.org>
2013-12-12 17:12:10 +0100

    update XARG class

--- src/l2/cxarg.cpp
+++ src/l2/cxarg.cpp
@@ -1,3 +1,24 @@
+/*
+    ---------------------------------------------------------------------------------------------------------------------------------------
+    (C)2013 Thomas AUGUEY <contact@aceteam.org>
+    ---------------------------------------------------------------------------------------------------------------------------------------
+    This file is part of WebFrameWork.
+
+    WebFrameWork is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebFrameWork is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebFrameWork.  If not, see <http://www.gnu.org/licenses/>.
+    ---------------------------------------------------------------------------------------------------------------------------------------
+*/
+
 #include "cxarg.h"
 
 XARG::XARG(const char* str, bool trim){
@@ -7,6 +28,12 @@ XARG::XARG(const char* str, bool trim){
 	char value[200];
 	while(*str != '\0'){
 		str = xarg_decode_field(str,name,value);
+		if(trim){
+            char* begin=name;
+            char* end=name+strlen(name);
+            str_trim(&begin,&end);
+            *end=0;
+		}
 		this->insert( std::pair<std::string,std::string>(std::string(name),std::string(value)) );
 	}
 }
@@ -16,8 +43,9 @@ XARG::XARG(const char* str, bool trim){
 */
 #ifdef GTEST
 TEST(XARG, XARG) {
-	char expected[]={'h','e','l','l','o',XARG_START_OF_TEXT_CODE,'w','o','r','l','d',XARG_END_OF_TEXT_CODE,0};
-    XARG xarg(expected);
-	//printf(xarg.at("hello"));
+    char text[]={'h','e','l','l','o',XARG_START_OF_TEXT_CODE,'w','o','r','l','d',XARG_END_OF_TEXT_CODE,'f','o','o',XARG_START_OF_TEXT_CODE,'b','a','r',XARG_END_OF_TEXT_CODE,0};
+    XARG xarg(text);
+    ASSERT_STREQ("world", xarg["hello"].c_str()) << "Should be equal";
+    ASSERT_STREQ("bar", xarg["foo"].c_str()) << "Should be equal";
 }
 #endif
--- src/l2/cxarg.h
+++ src/l2/cxarg.h
@@ -1,13 +1,43 @@
+/*
+    ---------------------------------------------------------------------------------------------------------------------------------------
+    (C)2013 Thomas AUGUEY <contact@aceteam.org>
+    ---------------------------------------------------------------------------------------------------------------------------------------
+    This file is part of WebFrameWork.
+
+    WebFrameWork is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebFrameWork is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebFrameWork.  If not, see <http://www.gnu.org/licenses/>.
+    ---------------------------------------------------------------------------------------------------------------------------------------
+*/
+
 #ifndef _NP_L2_XARG
 #define _NP_L2_XARG
 
 #include "../l1/xarg.h"
+#include "../l1/str.h"
 #include <stdio.h>
 #include <iostream>
 #include <map>
 
+/**
+    @brief Tableau associatif initialisable avec une chaine XARG
+*/
 class XARG : public std::map<std::string, std::string>{
 public:
+    /**
+        @brief Contructeur
+        @param str Chaine au format XARG
+        @param trim Supprime les caractères invisible de début et fin pour chaque clés
+    */
     XARG(const char* str, bool trim=true);
 };
 
